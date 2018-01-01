fe8e34e7890cc7768d554daa1f7e53a4b4af6f8a
Gunhee Lee <gunheecs.lee@samsung.com>
2018-01-01 20:38:27 +0900

    - Introduced Board and User class according to MVC pattern. - Thinking about what kind of DB we will use.

---
+++ src/model/Board.h
@@ -0,0 +1,11 @@
+#ifndef _board_h_
+#define _board_h_
+
+class Board
+{
+public:
+
+private:
+	User owner_;
+}
+#endif
---
+++ src/model/User.h
@@ -0,0 +1,10 @@
+#ifndef _user_h_
+#define _user_h_
+
+class User
+{
+public:
+
+private:
+}
+#endif
